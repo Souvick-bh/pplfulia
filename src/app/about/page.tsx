d8fa41dc096feb7851584d22959441c2d3be51c0
Souvick-bh <souvickb30@gmail.com>
2025-07-14 22:53:57 +0530

    Fixed about page single quote issue

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -26,22 +26,22 @@ export default function About() {
                 <div>
                     <h1 className="text-4xl lg:text-5xl mt-15 text-shadow-lg/30  text-shadow-[#ea5e00] ">From Gallis to Glory</h1>
                     <div className="text-2xl lg:text-3xl mt-10 ">What started as a friendly neighborhood tournament quickly turned
-                        into an annual festival of chaos, charisma, and cricket. We’ve had:
+                        into an annual festival of chaos, charisma, and cricket. We ave had:
                         <div className="flex flex-col items-center mt-10 text-[#ea5e00]"><div>Match-fixing allegations</div>
                         <div>Rain-interrupted matches</div>
                         <div>MVP awards</div></div>
                     </div>
-                    <div className="text-2xl lg:text-3xl mt-10 ">And guess what? We’re not just still going — we’re growing! This year marks our <span className="text-[#ea5e00]">6th Season</span>,
+                    <div className="text-2xl lg:text-3xl mt-10 ">And guess what? We are not just still going — we are growing! This year marks our <span className="text-[#ea5e00]">6th Season</span>,
                         and the spirit is crazier than ever. More teams, more sledging, and hopefully, fewer torn ligaments.</div>
                 </div>
 
                 <div className="text-3xl lg:text-4xl mt-20 ">
-                    We’re now building this website to preserve those memories, honor those classic moments, and give our beloved league the stage it deserves. Because one day, we’ll look back at this and say:
-                    <span className="text-[#ea5e00]">"Remember when we thought we were IPL stars?"</span>
+                    We are now building this website to preserve those memories, honor those classic moments, and give our beloved league the stage it deserves. Because one day, we will look back at this and say:
+                    <span className="text-[#ea5e00]">Remember when we thought we were IPL stars?</span>
                 </div>
 
                 <div className="text-3xl lg:text-4xl mt-20 mb-30">
-                    Browse through old photos, match reports, bloopers, and the drama that only true PPL fans understand. Whether you're one of us or just here to laugh at us —
+                    Browse through old photos, match reports, bloopers, and the drama that only true PPL fans understand. Whether you are one of us or just here to laugh at us —
                     Welcome to Panchayet Premiere League.
                     Where friendship meets fierce competition... 
                 </div>
